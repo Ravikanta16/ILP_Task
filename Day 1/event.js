d9ef555480786bc0fc0d74303ba767f3b526b5af
Ravikanta <ravikantagarg1999@gmail.com>
2025-05-25 19:30:25 +0530

    Day 1 Tasks

---
+++ Day 1/event.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("clickBtn");
+
+  button.addEventListener("click", function () {
+    alert("Button clicked!");
+    console.log("Button clicked!");
+  });
+});
